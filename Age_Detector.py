15a4ffe7eda73b2ffbca7fe5c93d44fdbb5e2dfa
Spypie60 <117548673+Spypie60@users.noreply.github.com>
2022-12-01 19:18:07 -0800

    Add files via upload

---
+++ Age_Detector.py
@@ -0,0 +1,19 @@
+
+Thing = "Congrats you are alomost"
+Month = 11 #The month nummber
+Month_time = 12 #your birthday
+
+year =2022
+
+age = 2000+year #Replace 2000 to your birth date
+age =year-2000 #Replace 2000 to your birth date
+if Month_time:
+    range (10, 12)
+if Month_time:
+    12 
+    age = age+1
+    print (age)
+else:
+          print (Thing) 
+          print (age)
+  
